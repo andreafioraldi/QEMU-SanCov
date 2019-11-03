a779e94e883306dd8776983504380a1d35c00520
Andrea Fioraldi <andreafioraldi@gmail.com>
2019-11-03 03:35:53 +0100

    fix -ldl

--- build_qemu.sh
+++ build_qemu.sh
@@ -107,7 +107,8 @@ echo "[+] Patching done."
 
 if [ "$STATIC" = "1" ]; then
 
-  CFLAGS="-O3 -ggdb" LDFLAGS="-ldl" ./configure --disable-bsd-user --disable-guest-agent --disable-strip --disable-werror \
+  CFLAGS="-O3 -ggdb -Wl,--no-as-needed -ldl" ./configure \
+    --extra-ldflags=-ldl --disable-bsd-user --disable-guest-agent --disable-strip --disable-werror \
 	  --disable-gcrypt --disable-debug-info --disable-debug-tcg --enable-docs --disable-tcg-interpreter \
 	  --enable-attr --disable-brlapi --disable-linux-aio --disable-bzip2 --disable-bluez --disable-cap-ng \
 	  --disable-curl --disable-fdt --disable-glusterfs --disable-gnutls --disable-nettle --disable-gtk \
@@ -124,9 +125,10 @@ else
   # --enable-pie seems to give a couple of exec's a second performance
   # improvement, much to my surprise. Not sure how universal this is..
   
-  CFLAGS="-O3 -ggdb" LDFLAGS="-ldl" ./configure --disable-system \
+  CFLAGS="-O3 -ggdb -Wl,--no-as-needed -ldl" ./configure --disable-system \
     --enable-linux-user --disable-gtk --disable-sdl --disable-vnc \
-    --target-list="${CPU_TARGET}-linux-user" --enable-pie --enable-kvm $CROSS_PREFIX || exit 1
+    --target-list="${CPU_TARGET}-linux-user" --enable-pie --enable-kvm \
+    --extra-ldflags=-ldl $CROSS_PREFIX || exit 1
 
 fi
 
