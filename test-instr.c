909ca36675f2d236eccfb3622fea656147a39ca1
Andrea Fioraldi <andreafioraldi@gmail.com>
2019-11-03 11:19:17 +0100

    test-instr indirect call

--- test-instr.c
+++ test-instr.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
+void win(void)
+{
+    printf("win\n");
+}
+
+void lose(void)
+{
+    return;
+}
+
 int main(int argc, char** argv)
 {
     if (argc < 10) {
         if (argc == 2) {
-            if (argv[1][0] == '0') printf("lol\n");
+            void (*fn)(void);
+            if (argv[1][0] == '0') fn = win;
+            else fn = lose;
+            fn();
         }
     } 
 }
